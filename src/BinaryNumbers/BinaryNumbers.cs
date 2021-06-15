29f8ed6b7284536c3c977ceb62bdc24f41bab5ba
Aske Wachs <askw@itu.dk>
2021-06-15 15:43:36 +0200

    int to binary algorithm implemented

--- src/BinaryNumbers/BinaryNumbers.cs
+++ src/BinaryNumbers/BinaryNumbers.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Text;
 
 namespace BinaryNumbers
 {
@@ -6,7 +8,23 @@ namespace BinaryNumbers
     {
         public string IntegerToBinary(int i)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+
+            var num = i;
+
+            do
+            {
+                var bin = num % 2;
+                sb.Append(bin.ToString());
+                num = (int) Math.Floor(num / 2.0);
+            } while (num > 0);
+
+            while (sb.Length < 32)
+            {
+                sb.Append('0');
+            }
+
+            return new string(sb.ToString().Reverse().ToArray());
         }
 
         public int BinaryToInteger(string s)
