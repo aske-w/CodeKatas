d6747f5c18fe7dd97d2034082ca567d6737470cc
Aske Wachs <askw@itu.dk>
2021-06-15 21:19:01 +0200

    caesar cipher test fix

--- src/CaesarCipher.Tests/CaesarCipherTestsPart2.cs
+++ src/CaesarCipher.Tests/CaesarCipherTestsPart2.cs
@@ -24,14 +24,14 @@ namespace CaesarCipher.Tests
         [Test]
         public void CaesarCipher_decodes_hello_world_with_4_shift()
         {
-            var result = caesarCipher.Encode("Lipps, Asvph!", 4);
+            var result = caesarCipher.Decode("Lipps, Asvph!", 4);
             Assert.AreEqual("Hello, World!", result);
         }
 
         [Test]
         public void CaesarCipher_dncode_does_not_alter_numbers()
         {
-            var result = caesarCipher.Encode("BCD123", 1);
+            var result = caesarCipher.Decode("BCD123", 1);
             Assert.AreEqual("ABC123", result);
         }
     }
