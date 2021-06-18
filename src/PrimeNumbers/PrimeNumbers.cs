0937e4342ce28aced73adc6d6138a492429ac246
Aske Wachs <askw@itu.dk>
2021-06-18 23:54:52 +0200

    prime number algorithm

--- src/PrimeNumbers/PrimeNumbers.cs
+++ src/PrimeNumbers/PrimeNumbers.cs
@@ -6,7 +6,30 @@ namespace PrimeNumbers
     {
         public int GetNextPrimeNumber(int i)
         {
-            throw new NotImplementedException();
+            int num = i + 1;
+            while (!IsPrime(num))
+            {
+                num++;
+            }
+            return num;
+        }
+
+        
+        private bool IsPrime(int n)
+        {
+            if (n == 2 || n == 3)
+                return true;
+
+            if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
         }
     }
 }
